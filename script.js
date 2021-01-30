912b501642e2d33ff4e50e1e326d985c40e21ddf
Hossain <hossainrabbi259@gmail.com>
2021-01-30 10:04:35 +0600

    all finish

--- script.js
+++ script.js
@@ -10,11 +10,12 @@ function handleFlyTicket(ticket, isIncrement) {
   }
   ticketInput.value = ticketNewCount;
 
-  subtotal();
+  totalAmount();
+  fullAmount(ticket, ticketNewCount);
 }
 
-// Subtotal
-function subtotal() {
+// Total Amount Calculated
+function totalAmount() {
   const firstClassCount = getCount('first-class');
   const economyCount = getCount('economy');
   const subtotal = firstClassCount * 150 + economyCount * 100;
@@ -23,10 +24,38 @@ function subtotal() {
   const tax = Math.round((subtotal / 100) * (100 + 10) - subtotal);
   document.getElementById('tax').innerText = tax;
 
-  document.getElementById('total').innerText = subtotal + tax;
+  return (document.getElementById('total').innerText = subtotal + tax);
 }
 
 function getCount(ticket) {
   const ticketInput = document.getElementById(ticket + '-input');
   return parseInt(ticketInput.value);
+}
+
+// extra
+function fullAmount(ticket, ticketCount) {
+  if (ticket == 'first-class') {
+    document.getElementById('first-class-value').innerText = ticketCount;
+    document.getElementById('first-class-total').innerText = ticketCount * 150;
+  }
+  if (ticket == 'economy') {
+    document.getElementById('economy-value').innerText = ticketCount;
+    document.getElementById('economy-total').innerText = ticketCount * 100;
+  }
+  document.getElementById('full-amount').innerText = totalAmount();
+}
+
+// Display Show and Hide
+function orderTicketBtn() {
+  displayProperty('block');
+}
+
+function popupHide() {
+  displayProperty('none');
+}
+
+function displayProperty(display) {
+  document
+    .getElementsByClassName('full-amount-area')[0]
+    .setAttribute('style', 'display:' + display);
 }
